41c3b39788054b97e5fb82815dbb5b9af957a0ac
qmamushroof <qmamushroof@gmail.com>
2025-06-25 14:02:42 +0600

    Update identity management to use custom ApplicationUser
    Modified ApplicationDbContext to utilize a custom ApplicationUser class, allowing for additional user properties. Updated _LoginPartial.cshtml to ensure SignInManager and UserManager work with the new user model.

--- MiniAccountManagementSystem/Data/ApplicationDbContext.cs
+++ MiniAccountManagementSystem/Data/ApplicationDbContext.cs
@@ -1,9 +1,10 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using MiniAccountManagementSystem.Models;
 
 namespace MiniAccountManagementSystem.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
