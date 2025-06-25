c9301ce2b1a5364801a1fa23ca41fff44c18a05f
qmamushroof <qmamushroof@gmail.com>
2025-06-25 13:53:28 +0600

    Add EF Core and Identity role management in Program.cs
    Implement Entity Framework Core services for database context and identity management. Retrieve SQL Server connection string and configure identity roles. Introduce `SeedRoles` method for role creation. Update HTTP request pipeline to include authentication and authorization middleware.

--- MiniAccountManagementSystem/Program.cs
+++ MiniAccountManagementSystem/Program.cs
@@ -5,7 +5,6 @@ using MiniAccountManagementSystem.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
@@ -23,7 +22,12 @@ builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+using (var scope = app.Services.CreateScope())
+{
+    var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+    await SeedRoles(roleManager);
+}
+
 if (app.Environment.IsDevelopment())
 {
     app.UseMigrationsEndPoint();
@@ -31,7 +35,6 @@ if (app.Environment.IsDevelopment())
 else
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -40,6 +43,7 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapRazorPages();
@@ -57,4 +61,3 @@ static async Task SeedRoles(RoleManager<IdentityRole> roleManager)
         }
     }
 }
-
